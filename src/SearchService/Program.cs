f9754b4db1f88e200d30e7383bcdad65b4cd5758
Tomás Ferreira <tomasfdev@outlook.com>
2024-02-02 20:17:27 +0000

    RabbitMQ config & contracts

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,4 @@
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService.Data;
@@ -9,6 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x =>    //MassTransit configuration
+{
+	x.UsingRabbitMq((context, cfg) =>
+	{
+		cfg.ConfigureEndpoints(context);
+	});
+});
 
 var app = builder.Build();
 
