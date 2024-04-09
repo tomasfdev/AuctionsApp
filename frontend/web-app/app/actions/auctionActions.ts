5fb8666845481f73734e91367811dd9a5907bbae
Tomás Ferreira <tomasfdev@outlook.com>
2024-04-09 20:26:21 +0100

    ClientApp CRUD

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,37 +1,39 @@
 "use server";
 
 import { Auction, PagedResult } from "@/types";
-import { getTokenWorkaround } from "./authActions";
+import { fetchWrapper } from "@/lib/fetchWrapper";
+import { FieldValues } from "react-hook-form";
+import { revalidatePath } from "next/cache";
 
 export async function getAuctions(
   query: string
 ): Promise<PagedResult<Auction>> {
-  const result = await fetch(`http://localhost:6001/search${query}`);
-
-  if (!result.ok) throw new Error("Failed to fetch data");
-
-  return result.json();
+  return await fetchWrapper.get(`search${query}`);
 }
 
-export async function UpdateAuctionTest() {
+export async function updateAuctionTest() {
   const data = { mileage: Math.floor(Math.random() * 100000) + 1 };
 
-  const token = await getTokenWorkaround();
-
-  const response = await fetch(
-    "http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c",
-    {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json",
-        Authorization: "Bearer " + token?.access_token,
-      },
-      body: JSON.stringify(data),
-    }
+  return await fetchWrapper.put(
+    "auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c",
+    data
   );
+}
 
-  if (!response.ok)
-    return { status: response.status, message: response.statusText };
+export async function createAuction(data: FieldValues) {
+  return await fetchWrapper.post("auctions", data);
+}
 
-  return response.statusText;
+export async function getDetailedViewData(id: string): Promise<Auction> {
+  return await fetchWrapper.get(`auctions/${id}`);
+}
+
+export async function updateAuction(data: FieldValues, id: string) {
+  const res = await fetchWrapper.put(`auctions/${id}`, data);
+  revalidatePath(`/auctions/${id}`);
+  return res;
+}
+
+export async function deleteAuction(id: string) {
+  return await fetchWrapper.del(`auctions/${id}`);
 }
